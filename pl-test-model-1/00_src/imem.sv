e9df35278ac3d2591b12186f21fb438d6f8f4fd3
DThien111 <123047643+DThien111@users.noreply.github.com>
2025-12-02 10:52:52 +0700

    Refactor imem module for clarity and efficiency
    Updated the imem module to improve clarity and functionality. Removed unnecessary comments and streamlined the synchronous read logic.

--- pl-test-model-1/00_src/imem.sv
+++ pl-test-model-1/00_src/imem.sv
@@ -1,37 +1,22 @@
 //===================================================================================
 // Module: imem (Instruction Memory)
-// Description: Synchronous Read-Only Memory for Pipeline Fetch Stage
+// Description: A wrapper for the generic memory module, configured for read-only
+//              instruction fetching. It takes a 32-bit byte address (PC) and
+//              outputs the corresponding 32-bit instruction.
 //===================================================================================
 
-// Cuối module này có 1 MUX để chọn giữa instr và NOP (0x00000013) phòng khi có flush
-// Stall thì giữ nguyên i_pc ko đổi
-
 module imem(
-    input  logic        i_clk,     	
-    input  logic        i_reset,		//cần reset hem
-    input  logic [31:0] i_pc,      
-    output logic [31:0] o_instr		//nếu đồng bộ thì ngõ ra instr có cần phải qua thanh ghi IF/ID ko, hay nhảy dô control unit luôn
+	 input  logic [31:0] i_pc,
+    output logic [31:0] o_instr
 );
-
-    logic [31:0] inst_mem [0:16383]; 
-    logic [13:0] pc_word;
-
-    /*initial begin
-        $readmemh("./../02_test/isa.mem", inst_mem); 
-    end*/
-
-    assign pc_word = i_pc[15:2];
-
-    //========================================================
-    // SYNCHRONOUS READ
-    //========================================================
-	 
-    always_ff @(posedge i_clk or negedge i_reset) begin
-		 if (!i_reset) begin
-				o_instr <= 32'b0;
-		 end else begin
-				o_instr <= inst_mem[pc_word]; 
-		 end
-	 end
-
+	logic [31:0] inst_mem [0:16383];
+	logic [13:0] pc_word;
+	
+	//initial begin
+		//$readmemh("./../02_test/application_ver2.hex", inst_mem); //cả 2 file ok
+	//end
+	
+	assign pc_word = i_pc[15:2];
+	
+	assign o_instr = inst_mem[pc_word];
 endmodule
