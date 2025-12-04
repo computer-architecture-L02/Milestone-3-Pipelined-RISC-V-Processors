b0224c409945f7a1c7865e1f7ca9a6582019eb91
DThien111 <123047643+DThien111@users.noreply.github.com>
2025-12-04 10:24:07 +0700

    Update pipelined.sv

--- pl-test-model-1/00_src/pipelined.sv
+++ pl-test-model-1/00_src/pipelined.sv
@@ -54,7 +54,7 @@ module pipelined (
 	 // Data Signals
 	 
 	 // IF
-	 logic [31:0] pc_four;
+	 logic [31:0] pc_four_if;
 	 logic [31:0] pc_next;
 	 logic 		  pc_sel;
 	 
@@ -87,38 +87,77 @@ module pipelined (
 	 logic [31:0] operand_a;
 	 logic [31:0] operand_b;
 	 logic [31:0] alu_data_ex;
+	 logic 		  mispred_ex;
 	 
 	 // MEM
-	 logic [31:0] wb_data;
 	 logic [31:0] alu_data_mem;
 	 logic [31:0] rs2_data_mem;
 	 logic [4:0]  rd_addr_mem;
-	 logic [31:0] instr_mem;
 	 logic 		  rd_wren_mem;
 	 logic 		  mem_ren_mem;
 	 logic 		  mem_wren_mem;
 	 logic 		  wb_sel_mem;
 	 logic 		  is_ctrl_mem;
 	 logic 		  insn_vld_mem;
+	 logic [31:0] ld_data_mem;
+	 logic [31:0] io_ledr_mem;
+    logic [31:0] io_ledg_mem;
+    logic [6:0]  io_hex0_mem, io_hex1_mem, io_hex2_mem, io_hex3_mem;
+    logic [6:0]  io_hex4, io_hex5_mem, io_hex6_mem, io_hex7_mem;
+    logic [31:0] io_lcd_mem;
+	 logic [31:0] pc_four_mem;
+	 logic 		  mispred_mem;
 	 
-
-	 //======================================================
-	 // 						HAZARD DETECTION UNIT 
-	 //======================================================
-  // mai làm
-  
+	 // WB
+	 logic [31:0] wb_data;
+    logic [31:0] pc_wb;
+    logic [31:0] pc_four_wb;
+    logic [31:0] alu_data_wb;
+    logic [31:0] ld_data_wb;
+    logic [4:0]  rd_addr_wb;
+    logic        rd_wren_wb;
+    logic [1:0]  wb_sel_wb;
+	 
+	 // ======================================================
+    // 						HAZARD DETECTION INSTANCE
+    // ======================================================
+    
+    hazard_detection_unit u_hazard (
+        // Inputs from ID
+        .i_rs1_addr     (instr_id[19:15]),
+        .i_rs2_addr     (instr_id[24:20]),
+        
+        // Inputs from EX
+        .i_rd_addr_ex   (rd_addr_ex),
+        .i_rd_wren_ex   (rd_wren_ex),
+        
+        // Inputs from MEM
+        .i_rd_addr_mem  (rd_addr_mem),
+        .i_rd_wren_mem  (rd_wren_mem),
+        
+        // Input from BRC (Branch Taken)
+        .i_branch_taken (pc_sel), //lấy từ brc
+        
+        // Outputs
+        .o_stall_pc     (stall_pc),
+        .o_stall_if_id  (stall_if_id),
+        .o_stall_id_ex  (stall_id_ex),
+        .o_flush_if_id  (flush_if_id),
+        .o_flush_id_ex  (flush_id_ex)
+    );
+	 
 	 //======================================================
 	 // 						INSTRUCTION FETCH 
 	 //======================================================
 	 
-	 pcplus4 pc_plus_four(
+	 pcplus4 pc_plus_4_if(
 		  .i_pc (pc_if),
-		  .o_pc_four (pc_four)
+		  .o_pc_four (pc_four_if)
 	 );
 	 
 	 pcsel pcsel(
-		  .i_alu_data   (alu_data),
-		  .i_pc_four    (pc_four),
+		  .i_alu_data   (alu_data_ex),
+		  .i_pc_four    (pc_four_if),
 		  .i_pc_sel     (pc_sel),
 		  .o_pc_next    (pc_next)
 	 );
@@ -147,7 +186,7 @@ module pipelined (
 		  
 		  .o_pc				(pc_id),
 		  .o_instr			(instr_id),
-		  .o_insn_vld		(insn_vld_id),
+		  .o_insn_vld		(insn_vld_id)
 	 );
 	 
 	 //======================================================
@@ -161,15 +200,14 @@ module pipelined (
         .i_rs2_addr (instr_id[24:20]), // Extract rs2 address
         .o_rs1_data (rs1_data_id),
         .o_rs2_data (rs2_data_id),
-        .i_rd_addr  (instr_id[11:7]),  // Extract rd address
+        .i_rd_addr  (rd_addr_wb),  // Extract rd address
         .i_rd_data  (wb_data),
-        .i_rd_wren  (ctrl_rd_wren) // chỉnh lại cái này
+        .i_rd_wren  (rd_wren_wb)
     );
 	 
 	 control_unit u_control_unit (
 			.i_instr			(instr_id),
 			.o_rd_wren		(ctrl_rd_wren),    
-			.o_insn_vld		(insn_vld_id),  
 			.o_br_un			(ctrl_br_un),      
 			.o_opa_sel		(ctrl_opa_sel),    
 			.o_opb_sel		(ctrl_opb_sel),    
@@ -182,7 +220,7 @@ module pipelined (
 	
 	ImmGen immgen(
 			.i_instr			(instr_id),
-			.o_ImmExt 		(ImmExt_id),
+			.o_ImmExt 		(ImmExt_id)
 	);
 	
 	// ID/EX Register
@@ -241,7 +279,7 @@ module pipelined (
 			  .i_pc				(pc_ex),
 			  .i_rs1_data		(rs1_data_ex),
 			  .i_opa_sel		(opa_sel_ex),
-			  .o_operand_a		(operand_a),
+			  .o_operand_a		(operand_a)
 	 );
 	 
 	 opbsel opbsel(
@@ -261,9 +299,12 @@ module pipelined (
 	 brc brc(
 			  .i_rs1_data	(rs1_data_ex),
 			  .i_rs2_data	(rs2_data_ex),
+			  .i_br_un		(br_un_ex),
 			  .i_instr		(instr_ex),
 			  .o_pc_sel		(pc_sel)
 	 );
+	 
+	 assign mispred_ex = pc_sel & is_ctrl_ex;
 	 
 	 // EX/MEM Register
 	 EX_MEM EXMEM(
@@ -282,7 +323,7 @@ module pipelined (
 			 .i_wb_sel		(wb_sel_ex),
 			 .i_ctrl			(is_ctrl_ex),        
 			 .i_insn_vld	(insn_vld_ex),
-			 .i_mispred		(),
+			 .i_mispred		(mispred_ex),
 			 
 			 .o_pc			(pc_mem),
 			 .o_alu_data	(alu_data_mem),
@@ -295,21 +336,116 @@ module pipelined (
 			 .o_wb_sel		(wb_sel_mem),
 			 .o_ctrl			(is_ctrl_mem),
 			 .o_insn_vld	(insn_vld_mem),
-			 .o_mispred		()
+			 .o_mispred		(mispred_mem)
 	 );
 	 
 	 
 	 //======================================================
 	 // 						MEMORY 
-	 //======================================================
+	 //======================================================    
+
+    lsu lsu (
+        .i_clk      (i_clk),
+        .i_reset    (i_reset),
+        
+        // Memory Interface
+        .i_lsu_addr (alu_data_mem),
+        .i_st_data  (rs2_data_mem),
+        .i_lsu_wren (mem_wren_mem),
+        .i_funct3   (instr_mem[14:12]),
+        .o_ld_data  (ld_data_mem),     
+        
+        // I/O Interface (Nối ra ngoài module pipelined)
+        .o_io_ledr  (io_ledr_mem),
+        .o_io_ledg  (io_ledg_mem),
+        .o_io_hex0  (io_hex0_mem), 
+		  .o_io_hex1  (io_hex1_mem),
+        .o_io_hex2  (io_hex2_mem), 
+		  .o_io_hex3  (io_hex3_mem),
+        .o_io_hex4  (io_hex4_mem), 
+		  .o_io_hex5  (io_hex5_mem),
+        .o_io_hex6  (io_hex6_mem), 
+		  .o_io_hex7  (io_hex7_mem),
+        .o_io_lcd   (io_lcd_mem),
+        .i_io_sw    (i_io_sw)
+    );
+
+	 pcplus4 pc_plus_4_mem(
+		  .i_pc		 (pc_mem),
+		  .o_pc_four (pc_four_mem)
+	 );
 	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
+    // MEM/WB Register
+    MEM_WB MEMWB (
+        .i_clk          (i_clk),
+        .i_reset        (i_reset),
+        
+        // Data Inputs
+        .i_pc           (pc_mem), // PC của lệnh hiện tại (để debug)
+        .i_pc_four      (pc_four_mem), // Tính PC+4 tại đây hoặc truyền từ trước
+        .i_alu_data     (alu_data_mem),
+        .i_ld_data      (ld_data_mem),
+        .i_rd_addr      (rd_addr_mem),
+        
+        // Control Inputs
+        .i_rd_wren      (rd_wren_mem),
+        .i_wb_sel       (wb_sel_mem),
+        .i_ctrl         (is_ctrl_mem),
+        .i_insn_vld     (insn_vld_mem),
+        .i_mispred      (mispred_mem),
+        
+        // I/O Inputs (Pass-through for debugging/display consistency if needed)
+        .i_io_ledr  (io_ledr_mem),
+        .i_io_ledg  (io_ledg_mem),
+        .i_io_hex0  (io_hex0_mem), 
+		  .i_io_hex1  (io_hex1_mem),
+        .i_io_hex2  (io_hex2_mem), 
+		  .i_io_hex3  (io_hex3_mem),
+        .i_io_hex4  (io_hex4_mem), 
+		  .i_io_hex5  (io_hex5_mem),
+        .i_io_hex6  (io_hex6_mem), 
+		  .i_io_hex7  (io_hex7_mem),
+        .i_io_lcd   (io_lcd_mem),
+		  
+		  .o_io_ledr  (o_io_ledr),
+        .o_io_ledg  (o_io_ledg),
+        .o_io_hex0  (o_io_hex0), 
+		  .o_io_hex1  (o_io_hex1),
+        .o_io_hex2  (o_io_hex2), 
+		  .o_io_hex3  (o_io_hex3),
+        .o_io_hex4  (o_io_hex4), 
+		  .o_io_hex5  (o_io_hex5),
+        .o_io_hex6  (o_io_hex6), 
+		  .o_io_hex7  (o_io_hex7),
+        .o_io_lcd   (o_io_lcd),
+        
+        // Outputs
+        .o_pc           (pc_wb),
+        .o_pc_four      (pc_four_wb),
+        .o_alu_data     (alu_data_wb),
+        .o_ld_data      (ld_data_wb),
+        .o_rd_addr      (rd_addr_wb),
+        .o_rd_wren      (rd_wren_wb),
+        .o_wb_sel       (wb_sel_wb),
+        
+        // Output Debug
+        .o_insn_vld     (o_insn_vld),
+        .o_ctrl         (o_ctrl),
+        .o_mispred      (o_mispred)
+    );
+    
+    assign o_pc_debug = pc_wb; // Output PC Debug lấy từ WB
+
+    // ======================================================
+    // 						WRITE BACK
+    // ======================================================
+    
+    wbsel wbsel (
+		  .i_pc_four(pc_four_wb),
+		  .i_alu_data(alu_data_wb),
+		  .i_ld_data(ld_data_wb),
+		  .i_wb_sel(wb_sel_wb),
+		  .o_wb_data(wb_data)
+	 );
 	 
 endmodule
